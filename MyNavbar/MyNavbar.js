68195285c8008d71a1dd074affd7f02cfb99c088
missnaziya <121482938+missnaziya@users.noreply.github.com>
2023-01-12 13:05:53 +0530

    Add files via upload

---
+++ MyNavbar/MyNavbar.js
@@ -0,0 +1,15 @@
+import {Route,Switch} from 'react-router-dom';
+import About from '../About';
+
+const MyNavbar = () =>{
+    return(
+        <>
+       {/* <Switch>
+        <Route path='/' component={About}></Route>
+       </Switch> */}
+        </>
+    )
+}
+export default MyNavbar;
+
+
